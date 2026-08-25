7df75bb66d00fa6c04cd7407a3ccd5cabda4f802
Yukkku <yukku.scratcher@gmail.com>
2026-08-25 09:50:50 +0900

    接続できるようにしたい

--- src/extensions/arduino/index.js
+++ src/extensions/arduino/index.js
@@ -1,4 +1,26 @@
+class Arduino {
+  #runtime;
+
+  constructor(runtime, extensionId) {
+    this.#runtime = runtime;
+    runtime.registerPeripheralExtension(extensionId, this);
+  }
+
+  scan() {}
+  connect() {}
+  disconnect() {}
+  isConnected() {}
+}
+
 class ArduinoBlocks {
+  #runtime;
+  #peripheral;
+
+  constructor(runtime) {
+    this.#runtime = runtime;
+    this.#peripheral = new Arduino(runtime, "arduino");
+  }
+
   getInfo () {
     return {
       id: "arduino",
